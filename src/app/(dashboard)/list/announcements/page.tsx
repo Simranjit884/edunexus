1c65343cc75b3f4bf245f9ca2b4cf78e5933bbd4
samy884 <ssgill884@gmail.com>
2025-08-12 09:50:54 +0530

    Enhanced role-based access control across various list pages by integrating Clerk authentication. Updated data fetching logic to utilize user roles for filtering announcements, events, exams, lessons, parents, results, students, and teachers. Refactored renderRow functions to accommodate new role-specific actions and improved overall user experience.

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -86,6 +86,21 @@ const AnnouncementListPage = async ({
     }
   }
 
+  // ROLE CONDITIONS
+
+  const roleConditions = {
+    teacher: { lessons: { some: { teacherId: currentUserId! } } },
+    student: { students: { some: { id: currentUserId! } } },
+    parent: { students: { some: { parentId: currentUserId! } } },
+  };
+
+  query.OR = [
+    { classId: null },
+    {
+      class: roleConditions[role as keyof typeof roleConditions] || {},
+    },
+  ];
+
   const [data, count] = await prisma.$transaction([
     prisma.announcement.findMany({
       where: query,
