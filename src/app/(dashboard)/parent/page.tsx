59bdb4726354ddb3d2941cb4d653d10127ffe525
samy884 <ssgill884@gmail.com>
2025-08-16 15:36:57 +0530

    Refactored Parent, Student, and Teacher pages to utilize BigCalendarContainer for dynamic schedule rendering based on user roles. Integrated Prisma for data fetching, ensuring schedules are tailored to the authenticated user. Added utility functions for adjusting lesson schedules to the current week, enhancing calendar accuracy.

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,15 +1,32 @@
 import Announcements from "@/components/Announcements";
-import BigCalendar from "@/components/BigCalender";
+import BigCalendarContainer from "@/components/BigCalendarContainer";
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
 
-const ParentPage = () => {
+const ParentPage = async () => {
+  const { userId } = await auth();
+  const currentUserId = userId;
+
+  const students = await prisma.student.findMany({
+    where: {
+      parentId: currentUserId!,
+    },
+  });
+
   return (
     <div className="flex flex-1 flex-col gap-4 p-4 xl:flex-row">
       {/* LEFT */}
       <div className="w-full xl:w-2/3">
-        <div className="h-full rounded-md bg-white p-4">
-          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
-          <BigCalendar />
-        </div>
+        {students.map((student) => (
+          <div className="w-full xl:w-2/3" key={student.id}>
+            <div className="h-full rounded-md bg-white p-4">
+              <h1 className="text-xl font-semibold">
+                Schedule ({student.name + " " + student.surname})
+              </h1>
+              <BigCalendarContainer type="classId" id={student.classId} />
+            </div>
+          </div>
+        ))}
       </div>
       {/* RIGHT */}
       <div className="flex w-full flex-col gap-8 xl:w-1/3">
